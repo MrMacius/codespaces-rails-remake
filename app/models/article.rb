ed16bf62f09343d22cd52f55deada267a8024921
MrMacius <103509543+MrMacius@users.noreply.github.com>
2023-06-19 21:26:40 +0000

    modified:   app/models/article.rb 	modified:   app/models/comment.rb 	modified:   app/models/tag.rb 	modified:   app/views/articles/show.html.erb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
     #belongs_to :User
     has_many :comments
-    has_and_belongs_to_many :Tag
+    #has_and_belongs_to_many :Tag
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,3 @@
 class Tag < ApplicationRecord
-    has_and_belongs_to_many :Article
+    #has_and_belongs_to_many :Article
 end
