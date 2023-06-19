40ffde2e39242ee3cd815b5a5c880fbdd93e5ccd
MrMacius <103509543+MrMacius@users.noreply.github.com>
2023-06-19 21:27:49 +0000

    modified:   app/controllers/comments_controller.rb 	modified:   app/views/articles/show.html.erb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,22 +7,24 @@ class CommentsController < ApplicationController
     def new
       @comment = Comment.new
     end
-    # def create
-    #   @article = Article.find(params[:article_id])
-    #   @comment = @article.comments.create(comment_params)
-    #   redirect_to article_path(@article)
-    # end
     def create
-      
-        @comment = Comment.new(comment_params.merge(article_id: 1))
-        if @comment.save
-            redirect_to root_path, notice: "COMMENT ADDED SUCCESFULLY"
-        else
-            #redirect_to public_home2_path, notice: "ERROR 2137"
-            flash[:alert] = "Something went wrong"
-            render :new
-      end
+      @article = Article.find(params[:article_id])
+      @comment = @article.comments.create(comment_params)
+      redirect_to article_path(@article)
     end
+
+
+    # def create
+      
+    #     @comment = Comment.new(comment_params.merge(article_id: 1))
+    #     if @comment.save
+    #         redirect_to root_path, notice: "COMMENT ADDED SUCCESFULLY"
+    #     else
+    #         #redirect_to public_home2_path, notice: "ERROR 2137"
+    #         flash[:alert] = "Something went wrong"
+    #         render :new
+    #   end
+    # end
 
     # def show
     #   @comment = Comment.find(params[:id])
@@ -30,8 +32,10 @@ class CommentsController < ApplicationController
 
     private
 
-      def comment_params
-        params.permit(:author, :content, :article_id)
-      end
-
+      # def comment_params
+      #   params.permit(:author, :content, :article_id)
+      # end
+    def comment_params
+      params.require(:comment).permit(:commenter, :body, :status)
+    end
 end
